322cd49d71c580be2d44a40f010a5e6687b542a2
dohlandrine <landrinedoh22@gmail.com>
2024-07-20 20:37:41 +0100

    Added a counter provider, at lib/provider/counter_provider.dart

---
+++ lib/provider/counter_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CounterProvider extends ChangeNotifier {
+  late int count;
+  CounterProvider({this.count = 0});
+  void increment() {
+    count++;
+    notifyListeners();
+  }
+
+  void decrement() {
+    count--;
+    notifyListeners();
+  }
+}
